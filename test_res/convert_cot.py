8db44d73acda28001450438fd31ccbe873aea1c0
root <root@h-aoji-aoji-h100-2node-w-0.h-aoji-aoji-h100-2node.hbox-ai.svc.hbox2-shcdt-prd.local>
2025-07-30 16:30:17 +0800

    update test_res/

--- test_res/convert_cot.py
+++ test_res/convert_cot.py
@@ -6,8 +6,11 @@ import json
 data = json.load(open(sys.argv[1]))
 res = []
 for i in data:
-    i["answer"] = re.findall(r'<answer>(.*?)</answer>', i["answer"], re.DOTALL)[0].strip()
-    res.append(i)
+    try:
+        i["answer"] = re.findall(r'<answer>(.*?)</answer>', i["answer"], re.DOTALL)[0].strip()
+        res.append(i)
+    except:
+        print(i["answer"])
 
 json.dump(res, open("cot_results.json", 'w'), indent=2, ensure_ascii=False)
 
